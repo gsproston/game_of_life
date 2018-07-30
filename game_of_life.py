05886b5f5d91301bbc7c400ab0540c9858601534
gsproston <gsproston94@gmail.com>
2018-07-30 23:14:45 +0100

    game logic

--- game_of_life.py
+++ game_of_life.py
@@ -1,10 +1,24 @@
-import pygame, sys
+import pygame, sys, time
 from pygame.locals import *
 
 pygame.init()
 
+Board = [[]]
+Board2 = [[]]
+GRIDSIZE = 10
+
 def draw(): #called when screen needs to be updated
     pygame.display.flip()
+    
+    # draw the board
+    for i in range(0, len(Board)):
+      for j in range(0, len(Board[i])):
+        if (Board[i][j] == 0):
+          # draw empty square
+          pygame.draw.rect(screen, (0,0,0), (i*GRIDSIZE,j*GRIDSIZE,GRIDSIZE,GRIDSIZE), 1)
+        else:
+          # draw filled square
+          pygame.draw.rect(screen, (0,0,0), (i*GRIDSIZE,j*GRIDSIZE,GRIDSIZE,GRIDSIZE))
     
 def wresize(): #screen is resized, recalculate some variables
     global cxpos,cypos
@@ -27,6 +41,40 @@ def resetFlags(): #new flags set
         else:
             flags = 0
     pygame.display.set_mode((w,h),flags)
+    
+def copyBoard():
+  for i in range(0, len(Board)):
+    for j in range(0, len(Board[i])):
+      Board[i][j] = Board2[i][j]
+    
+def updateBoard():
+  updated = False
+  for i in range(0, len(Board)):
+    for j in range(0, len(Board[i])):
+      updateCell(i,j)
+      updated = (Board[i][j] != Board2[i][j])
+  return updated
+    
+def updateCell(x, y):
+  # total holds the number of living cells
+  total = 0
+  # go through all surrounding cells
+  for i in range(-1, 2):
+    for j in range(-1, 2):
+      # ignore the centre square (that's us!)
+      if (i != 0 and j != 0):
+        try:
+          total += Board[x+i][y+j]
+          continue
+        except IndexError:
+          continue
+        
+  if (total < 2 or total > 3):
+    Board2[i][j] = 0
+  elif (total == 3):
+    Board2[i][j] = 1
+  else:
+    Board2[i][j] = Board[i][j]
     
 if __name__ == "__main__":
     #gets monitor info, used when resizing
@@ -57,15 +105,32 @@ if __name__ == "__main__":
     
     clock = pygame.time.Clock()
 
-    draw()
-
     #main game loop
     while (not shutdown):        
         for event in pygame.event.get(): #runs when an event occurs
             if event.type == QUIT: #quit called
                 shutdown = True #end loop
+                
+        # init the boards
+        boardw = 100;
+        boardh = 50;
+        Board = [[0 for x in range(boardh)] for y in range(boardw)]
+        Board2 = [[0 for x in range(boardh)] for y in range(boardw)]
+        
+        updated = True
+        # only keep drawing the board if the state changes
+        while (updated): 
+          draw()     
+        
+          updated = updateBoard()
+          if (updated):
+            print("TRUE")
+          else:
+            print("FALSE")
+          copyBoard()
 
-        clock.tick(144) #update x times a second, determines FPS
+          clock.tick(144) #update x times a second, determines FPS
+        shutdown = True
 
     #main loop ends, exit
     pygame.quit()    
